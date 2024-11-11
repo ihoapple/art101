bb10553f8cfe7e149c8434f84c8229c56bf5bc44
ihoapple <ihoapple@gmail.com>
2024-11-11 15:30:52 +0800

    lab11 3

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,7 +2,7 @@
  * lab.js - This simple JavaScript/jQuery script appends new elements to an output div
    Requirements: jQuery must be loaded for this script to work.
  * Author:Yi Ho
- * Created:11.7.2024
+ * Created:11.10.2024
  **/
 function sortString(inputString) {
     return inputString.split('').sort().join('');
